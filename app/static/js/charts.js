422fda4e39bc23bbc1b9730d2fe4aba54fd26654
mqiu30 <mqiu30@stuy.edu>
2023-05-08 13:17:08 -0400

    jdjd

--- app/static/js/charts.js
+++ app/static/js/charts.js
@@ -6,10 +6,28 @@ function fib(n){
         return (fib(n-1) + fib(n-2))
     }
   }
-  
+
+
   var addFib = function(text) {
-    var list = document.getElementById("foo"); //this is an HTML ID Tag. in index.html it is for the <ol> or ordered list
-    var newitem = document.createElement("li"); 
-    newitem.innerHTML = "The third term in the Fibonacci Sequence is " + fib(3); //we are adding a entry of the text inside the ordered list with the JS function
+    var list = document.getElementById("fiblist");
+    var newitem = document.createElement("li");
+    newitem.innerHTML = text;
     list.appendChild(newitem);
   };
+  
+
+//charts for economics 
+function 
+
+// FIB BUTTONs
+var fb = document.getElementById("fibbutton")
+fibbutton.addEventListener('click', fibaction)
+//addFib("works up to here")
+
+function fibaction() {
+  var ans = document.createElement("li")
+  //ans.innerHTML = fib(Number(document.getElementById('fibinput').getAttribute()))
+  ans.innerHTML= document.getElementById('fibinput').value + ": " + fib(Number(document.getElementById('fibinput').value))
+  document.getElementById("fiblist").appendChild(ans)
+  //addFib(fib(10))
+}
---
+++ app/static/js/world.js
@@ -0,0 +1,76 @@
+chart1 = {
+    const context = DOM.context2d(width, height);
+    const path = d3.geoPath(projection, context);
+  
+    function render(land) {
+      context.clearRect(0, 0, width, height);
+      context.beginPath(), path(sphere), context.fillStyle = "#fff", context.fill();
+      context.beginPath(), path(land), context.fillStyle = "#000", context.fill();
+      context.beginPath(), path(sphere), context.stroke();
+    }
+  
+    return d3.select(context.canvas)
+      .call(drag(projection)
+          .on("drag.render", () => render(land110))
+          .on("end.render", () => render(land50)))
+      .call(() => render(land50))
+      .node();
+  }
+
+  function drag(projection) {
+    let v0, q0, r0;
+    
+    function dragstarted() {
+      v0 = versor.cartesian(projection.invert([d3.event.x, d3.event.y]));
+      q0 = versor(r0 = projection.rotate());
+    }
+    
+    function dragged() {
+      const v1 = versor.cartesian(projection.rotate(r0).invert([d3.event.x, d3.event.y]));
+      const q1 = versor.multiply(q0, versor.delta(v0, v1));
+      projection.rotate(versor.rotation(q1));
+    }
+    
+    return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged);
+  }
+
+  projection = d3[projectionName]().precision(0.1)
+
+  height = {
+    const [[x0, y0], [x1, y1]] = d3.geoPath(projection.fitWidth(width, sphere)).bounds(sphere);
+    const dy = Math.ceil(y1 - y0), l = Math.min(Math.ceil(x1 - x0), dy);
+    projection.scale(projection.scale() * (l - 1) / l).precision(0.2);
+    return dy;
+  }
+
+  sphere = ({type: "Sphere"})
+  
+  land50 = Object {type: "FeatureCollection", features: Array(1)}
+  land110 = Object {type: "FeatureCollection", features: Array(1)}
+  versor = ƒ(t)
+  topojson = Object {bbox: ƒ(e), feature: ƒ(e, t), merge: ƒ(e), mergeArcs: ƒ(e, t), mesh: ƒ(e), meshArcs: ƒ(e, t, r), neighbors: ƒ(e), quantize: ƒ(e, t), transform: ƒ(e), untransform: ƒ(e)}
+  d3 = Object {event: null, format: ƒ(t), formatPrefix: ƒ(t, n), timeFormat: ƒ(t), timeParse: ƒ(t), utcFormat: ƒ(t), utcParse: ƒ(t), FormatSpecifier: ƒ(t), active: ƒ(t, n), arc: ƒ(), area: ƒ(), areaRadial: ƒ(), ascending: ƒ(t, n), autoType: ƒ(t), axisBottom: ƒ(t), axisLeft: ƒ(t), axisRight: ƒ(t), axisTop: ƒ(t), bisect: ƒ(n, e, r, i), bisectLeft: ƒ(n, e, r, i), …}
+  
+
+  
+  
+
+function sales(data){
+    const div = d3.create("div")
+       .style("font", "10px sans-serif")
+       .style("text-align", "right")
+       .style("color", "white");
+    
+    div.selectAll("div")
+       .data(data)
+       .join("div")
+       .style("background", "steelblue")
+       .style("padding", "3px")
+       .style("margin", "1px")
+       .style("width", d => `${d * 10}px`)
+       .text(d => d);
+    
+    return div.node();
+   }
